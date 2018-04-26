92f558edc4c81152052ab7645996b969bea237ec
elegant651 <korea651@gmail.com>
2018-04-27 04:51:46 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => next());
+
+require('./dialogflow')(app);
+
+const server = app.listen(3000, function(){
+  console.log("Express server has started on port 3000")
+});
